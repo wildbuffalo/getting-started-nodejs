d09f7064dc8a7bfb2e332c4d76c5dd01ae473d4a
Zengqiao Liu <feirenliu@gmail.com>
2018-10-11 13:01:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello dddfefedfedfedfdworld\n');
+    res.send('Hello dddfefefdworld\n');
 });
 
 app.listen(PORT, HOST);
