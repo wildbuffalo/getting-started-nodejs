a957b7873f1af4fd12a79173e7143d9ca1cf2b23
wildbuffalo <feirenliu@gmail.com>
2019-04-24 16:27:12 -0400

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var app = express();
-var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo"]}
-
-app.get('/', function(req, res){
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(cities));
-    res.end();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
-
-var port = process.env.PORT || 8080;
-app.listen(port);
-
-module.exports = app;
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
